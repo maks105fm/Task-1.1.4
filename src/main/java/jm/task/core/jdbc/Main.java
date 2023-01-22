de6f3cef3f80c64c28e88843d145caccf8209b88
maks105fmm <maks105fmm@gmail.com>
2023-01-22 17:45:54 +0300

    1.1.4.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -18,6 +18,7 @@ public class Main {
         userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
+        System.out.println();
       /*
 
   */
